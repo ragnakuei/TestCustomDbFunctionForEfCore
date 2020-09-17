0c1dbdc797fabcfed8f33ee1bc9d446b2440f1d6
Kuei <ragnakuei@livemail.tw>
2020-09-17 15:08:33 +0800

    Add project files.

---
+++ TestCustomDbFunctionForEfCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCustomDbFunctionForEfCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
